723d6c44a8ee5995e1416021ea2504177068bac9
mehrana1380 <84845533+mehrana1380@users.noreply.github.com>
2021-06-17 04:42:28 -0700

    Add files via upload

--- class_5.py
+++ class_5.py
@@ -1,2 +1,5 @@
-for adad in range(1,100,3):
-    print(adad)
+my_class= "paniz zeinab mahnaz mehrana mohammad amin farokh reza meysam".split('')
+for index, name in enumerate(my_class):
+    print (f'nafare shomare {index} {name} ast')
+    if index% 3== 0:
+        continue
